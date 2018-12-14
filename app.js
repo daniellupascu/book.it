6669ae3c618015c586dabe055e57bd0687c5b695
Daniel Lupascu <daniellupascu@Daniels-MacBook-Pro.local>
2018-12-14 22:30:16 +0100

    create the graphql initial schema and activating graphiql

--- app.js
+++ app.js
@@ -1,13 +1,34 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const graphqlHttp = require('express-graphql')
+const { buildSchema } = require('graphql')
+
 
 let app = express()
 
 app.use(bodyParser.json())
 
-app.get('/', (req, res, next) => {
-    res.send('Hello world!')
-})
+app.use('/graphql', graphqlHttp({
+    schema: buildSchema(`
+        type RootQuery {
+            events: [String!]!
+        }
+
+        type RootMutation {
+            createEvent(name: String): String
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+        events: () =>  ['coding', 'dreaming'],
+        createEvent: args => `Event created ${args.name}`
+    },
+    graphiql: true
+})) 
 
 app.listen(3000, () => {
     console.log('running on port 3000')
