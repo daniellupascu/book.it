dd14013c4fa5429a696f344b0b2e82083669da3f
Daniel Lupascu <daniellupascu@Daniels-MacBook-Pro.local>
2018-12-15 10:03:11 +0100

    impletent mongoDB with mongoose models

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const graphqlHttp = require('express-graphql')
 const { buildSchema } = require('graphql')
+const mongoose = require('mongoose')
 
+const Event = require('./models/event')
 
 let app = express()
 
@@ -10,12 +12,27 @@ app.use(bodyParser.json())
 
 app.use('/graphql', graphqlHttp({
     schema: buildSchema(`
+        type Event {
+            _id: ID
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
+        input EventInput {
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
         type RootQuery {
-            events: [String!]!
+            events: [Event!]!
         }
 
         type RootMutation {
-            createEvent(name: String): String
+            createEvent(eventInput: EventInput): Event
         }
 
         schema {
@@ -24,12 +41,45 @@ app.use('/graphql', graphqlHttp({
         }
     `),
     rootValue: {
-        events: () =>  ['coding', 'dreaming'],
-        createEvent: args => `Event created ${args.name}`
-    },
+        events: () =>  {
+            return Event.find().then(events => {
+                return events.map(event => {
+                    return { ...event._doc, _id: event.id }
+                })
+            }).catch(e => {
+                throw e
+            }) 
+        },
+        createEvent: args => {
+            // const event = {
+            //     _id: Math.random().toString(),
+            //     title: args.eventInput.title,
+            //     description: args.eventInput.description,
+            //     price: +args.eventInput.price,
+            //     date: args.eventInput.date,
+            // }
+            // events.push(event)
+            // return event
+
+            const event = new Event({
+                title: args.eventInput.title,
+                description: args.eventInput.description,
+                price: +args.eventInput.price,
+                date: new Date(args.eventInput.date),
+            })
+
+            return event.save().then(result => ({...result._doc, _id: event.id})).catch(e => {
+                console.log(e)
+                throw e
+            })
+
+        }
+    }, 
     graphiql: true
 })) 
 
-app.listen(3000, () => {
-    console.log('running on port 3000')
-})
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@bookit-a1klz.mongodb.net/test?retryWrites=true`, {useNewUrlParser: true}).then(() => {
+    app.listen(3000, () => {
+        console.log('running on port 3000')
+    })    
+}).catch(e => console.log(e))
